f65196a1a4847c4e6816317b60c29283a3fe29bf
Iván Peña Ramos <tpmivanpenha@gmail.com>
2026-03-22 18:02:29 +0100

    fix: remove generate_data task from DAG, add PYTHONPATH to airflow

--- dags/entsoe_raw_ingestion.py
+++ dags/entsoe_raw_ingestion.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-from spark.raw.generate_data import generate_data
 from spark.raw.write_raw import write_raw
 from spark.raw.verify_raw import verify_raw
 
@@ -12,8 +11,7 @@ with DAG(
     start_date=datetime(2024,1,1),
     catchup=False
 ) as dag:
-    task_1 = PythonOperator(task_id="generate_data", python_callable=generate_data, op_kwargs={"execution_date":"{{ ds }}"})
-    task_2 = PythonOperator(task_id="write_raw", python_callable=write_raw, op_kwargs={"execution_date":"{{ ds }}"})
-    task_3 = PythonOperator(task_id="verify_raw", python_callable=verify_raw, op_kwargs={"execution_date":"{{ ds }}"})
+    task_1 = PythonOperator(task_id="write_raw", python_callable=write_raw, op_kwargs={"execution_date":"{{ ds }}"})
+    task_2 = PythonOperator(task_id="verify_raw", python_callable=verify_raw, op_kwargs={"execution_date":"{{ ds }}"})
 
-    task_1 >> task_2 >> task_3
+    task_1 >> task_2
